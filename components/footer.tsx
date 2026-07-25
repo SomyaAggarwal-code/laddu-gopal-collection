f468acb9ca14fa90ed142a523bb1bf850b7e9943
SomyaAggarwal-code <somyaaggarwalu21@gmail.com>
2026-07-25 22:56:01 +0530

    Create footer.tsx

---
+++ components/footer.tsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#B8860B",
+        color: "white",
+        textAlign: "center",
+        padding: "20px",
+        marginTop: "40px",
+      }}
+    >
+      <h3>Laddu Gopal Collection</h3>
+
+      <p>📞 7015053081 | 9896000357</p>
+
+      <p>📍 Fatehabad, Haryana</p>
+
+      <p>Instagram: @laddu_gopal.collection</p>
+
+      <p>© 2026 Laddu Gopal Collection. All Rights Reserved.</p>
+    </footer>
+  );
+}
